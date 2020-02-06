f41e78d9cb4bd26a4996f17e51859ea372bde254
Alessandro Marrella <amarrella@earnestresearch.com>
2020-02-06 15:29:21 +0000

    Build some more stuff in CI

--- ci.nix
+++ ci.nix
@@ -1,6 +1,11 @@
 with (import ./.);
 
 {
+  fd = pkgs.fd;
+  hlint = pkgs.hlint;
+  git = pkgs.git;
+  ormolu = pkgs.ormolu;
   nix-tools = pkgs.haskell-nix.nix-tools;
+  stack = pkgs.stack;
   inherit (pkgs.haskell-nix.compiler) ghc865;
 }
