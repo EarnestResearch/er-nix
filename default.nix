eefddb90d89b8290bf713f1f957022ab40ae1c70
Gary Coady <gcoady@earnestresearch.com>
2020-04-15 16:45:11 +0200

    Refer to haskell-nix.nixpkgsArgs to align with upstream

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
   sources = import ./nix/sources.nix;
-  haskell-nix = import sources.haskell-nix;
+  haskell-nix = import sources.haskell-nix {};
 in
 rec {
   # A pinned version of nixpkgs, widely used and hopefully well cached.
@@ -13,7 +13,7 @@ rec {
   pkgs = pkgsForSystem builtins.currentSystem;
 
   # Args to apply to any nixpkgs to generate a package set with the overlays.
-  nixpkgsArgs = haskell-nix // { inherit overlays; };
+  nixpkgsArgs = haskell-nix.nixpkgsArgs // { inherit overlays; };
 
   # All the haskell.nix overlays plus all the er-nix overlays.
   overlays = haskell-nix.overlays ++ (import ./overlays);
