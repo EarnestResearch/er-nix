3f5adea974bd277a2ec737dfb4ccaf13210e91b1
Gary Coady <gcoady@earnestresearch.com>
2020-04-18 19:28:19 +0200

    Build ghc from pkgs.buildPackages.pkgs.haskell-nix.compiler
    This causes the ghc to be taken from the build platform, rather
    than from the target platform, this matters for cross-compilation.

--- ci.nix
+++ ci.nix
@@ -3,5 +3,5 @@ with (import ./.);
 
 {
   nix-tools = pkgs.haskell-nix.nix-tools;
-  ghc = builtins.getAttr ghc pkgs.haskell-nix.compiler;
+  ghc = builtins.getAttr ghc pkgs.buildPackages.pkgs.haskell-nix.compiler;
 }
