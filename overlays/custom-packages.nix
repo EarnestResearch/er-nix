d5db65c3214913010ba27d3b0489aa427b29da7d
Andrey Kartashov <andrey.kartashov@gmail.com>
2020-08-12 15:39:10 -0400

    point to project's nix derivation

--- overlays/custom-packages.nix
+++ overlays/custom-packages.nix
@@ -2,6 +2,6 @@ self: super:
 
 {
   aws-logs = self.callPackage ../pkgs/aws-logs {};
-  okta-aws-login = self.callPackage ../pkgs/okta-aws-login {};
+  okta-aws-login = import ../pkgs/okta-aws-login;
   skopeo = self.callPackage ../pkgs/skopeo {};
 }
--- pkgs/okta-aws-login/default.nix
+++ pkgs/okta-aws-login/default.nix
@@ -1,29 +1,11 @@
-{ fetchurl, lib, stdenv }:
-
 let
-  version = "1.3.2";
-  urlTemplate = arch: "https://github.com/EarnestResearch/okta-aws-login/releases/download/v${version}/okta-aws-login-${arch}";
-  sources = {
-    x86_64-linux = fetchurl {
-      url = urlTemplate "Linux-x86_64";
-      sha256 = "13fdixjv2x9ja4csflhij7namgbc7g0zjmzcy4p82qy5m7sxbh9v";
-    };
-    x86_64-darwin = fetchurl {
-      url = urlTemplate "Darwin-x86_64";
-      sha256 = "081mkzmzv6790rwnqprkwhfmrwc3vx3hs30r1pqx166667nzhwiw";
-    };
-  };
-  unsupported = throw "Platform ${stdenv.hostPlatform.system} unsupported for okta-aws-login";
+  oktaAwsLogin = import (
+    builtins.fetchGit {
+      url = "https://github.com/EarnestResearch/okta-aws-login.git";
+      ref = "refs/heads/master";
+      rev = "9bedc99e8abf7fab22738c5e2ccc8935b6a9f391";
+    }
+  );
+
 in
-stdenv.mkDerivation rec {
-  name = "okta-aws-login-${version}";
-  src = lib.attrByPath [ stdenv.hostPlatform.system ] unsupported sources;
-
-  dontUnpack = true;
-  dontFixup = true;
-
-  installPhase = ''
-    mkdir -p $out/bin
-    install -m 755 ${src} $out/bin/okta-aws-login
-  '';
-}
+(oktaAwsLogin {}).okta-aws-login.components.exes.okta-aws-login.out
