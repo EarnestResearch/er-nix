e86d4102d53ef739485ca2cd5c75d4db36408e73
Andrey Kartashov <andrey.kartashov@gmail.com>
2020-08-12 17:26:32 -0400

    Use niv, as suggested
    Update to latest okta-aws-login

--- pkgs/okta-aws-login/default.nix
+++ pkgs/okta-aws-login/default.nix
@@ -1,11 +1,5 @@
 let
-  oktaAwsLogin = import (
-    builtins.fetchGit {
-      url = "https://github.com/EarnestResearch/okta-aws-login.git";
-      ref = "refs/heads/master";
-      rev = "9bedc99e8abf7fab22738c5e2ccc8935b6a9f391";
-    }
-  );
-
+  sources = import ../../nix/sources.nix;
+  oktaAwsLogin = import sources.okta-aws-login;
 in
 (oktaAwsLogin {}).okta-aws-login.components.exes.okta-aws-login.out
