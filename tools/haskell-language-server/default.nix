5514c7e3c30acc1b3acbfc91738581c991dc170c
Ross A. Baker <ross@rossabaker.com>
2020-09-23 22:34:00 -0400

    Fetch submodule of haskell-language-server

--- tools/haskell-language-server/default.nix
+++ tools/haskell-language-server/default.nix
@@ -1,9 +1,19 @@
-{ sources }:
+{ fetchFromGitHub,
+  sources }:
 
 ghcVersion: import sources.nix-hls {
   inherit ghcVersion;
   sources = {
-    inherit (sources) haskell-language-server;
+    # This has a submodule, which niv doesn't yet handle.
+    # Note that this also defeats nix-prefetch-git.
+    # https://github.com/nmattia/niv/issues/229
+    haskell-language-server =
+      with sources.haskell-language-server;
+      fetchFromGitHub {
+        inherit owner repo rev sha256;
+        name = "haskell-language-server-src";
+        fetchSubmodules = true;
+      };
     "haskell.nix" = sources.haskell-nix;
   };
 }
