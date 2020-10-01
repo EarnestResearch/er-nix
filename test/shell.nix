9f468042036b501ce50f87e1e12e6508c8bf1e43
Ross A. Baker <ross@rossabaker.com>
2020-10-01 17:42:55 -0400

    ghcide -> hls

--- test/shell.nix
+++ test/shell.nix
@@ -2,14 +2,17 @@
 , compiler-nix-name
 }:
 let
-  pkgs = (import ../default.nix).pkgs;
+  er-nix = import ../default.nix;
+  pkgs = er-nix.pkgs;
   hsPkgs = import ./default.nix { inherit pkgs buildMusl compiler-nix-name; };
+  hls = er-nix.tools.haskell-language-server { project = hsPkgs.earnest-project; };
 in
 hsPkgs.shellFor {
   tools = {
     cabal = "3.2.0.0";
-    ghcide = "0.2.0";
   };
+
+  buildInputs = builtins.attrValues hls;
 
   exactDeps = true;
 }
